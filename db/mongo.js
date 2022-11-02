1dfaf4355e62c87f5592a9df26a284a26c7ad15a
Fran55sh <corvattafranco@gmail.com>
2022-11-01 23:04:38 -0300

    change db port

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const conection = mongoose.connect('mongodb://localhost:27017/dbmongo',{useNewUrlParser : true,useUnifiedTopology: true},()=>{
+const conection = mongoose.connect('mongodb://127.0.0.1:27017/dbmongo',{useNewUrlParser : true,useUnifiedTopology: true},()=>{
     console.log('successfully connected to database');
 });
 
