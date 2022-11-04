55a44aadbefb544eaf9e06e2fda6d623ce1bc22c
Fran55sh <corvattafranco@gmail.com>
2022-11-04 08:11:59 -0300

    delete fix

--- views/js/admin.js
+++ views/js/admin.js
@@ -8,7 +8,7 @@ async function eliminarCurso(id) {
     redirect: "follow",
   };
 
-  fetch(`http://localhost:3000/api/cursos/delete/${id}`, requestOptions)
+  fetch(`api/cursos/delete/${id}`, requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
@@ -36,10 +36,10 @@ function renderCursos(data) {
     newElement = document.createElement("div");
     newElement.classList.add("curso");
     newElement.innerHTML = `
-        <li> ${cursos.nombre}</li>
-        <img src="${cursos.thumbnail}" alt="">
-        <li> ${cursos.descripcion}</li>
-        <li> ${cursos.precio}</li>
+        <li> Nombre: ${cursos.nombre}</li>
+        <img class="thumbnail-listas-cursos" src="${cursos.thumbnail.slice(1)}" alt="">
+        <li class="descripcion-listas-cursos"> Descripcion <br>${cursos.descripcion}</li>
+        <li> Precio: ${cursos.precio}</li>
 
         <button onClick="eliminarCurso('${cursos._id}')">Eliminar</button>
         
