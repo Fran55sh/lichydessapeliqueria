92d285b6e8a37a06f25a9e4ae04c3b411cfacf87
fran55sh <corvattafranco@gmail.com>
2022-11-09 23:05:58 -0300

    contacto styles

--- db/mongo.js
+++ db/mongo.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose');
 const conection = mongoose.connect('mongodb://127.0.0.1:27017/dbmongo').
 catch(error => handleError(error));
 
-// Or:
 
 module.exports = conection;
