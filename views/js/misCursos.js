7a39130c267ad147c379581fbfa738af74e0f4cd
fran55sh <corvattafranco@gmail.com>
2022-11-16 01:00:51 -0300

    fix video thumbnail

--- views/js/misCursos.js
+++ views/js/misCursos.js
@@ -18,7 +18,7 @@
       newElement.innerHTML = `
             <div class="curso_container">
             <h2>${curso.cursoId.nombre}</h2>
-            <video id="videoPlayer" width="50%" controls muted="muted" poster="/assets/1.jpg" oncontextmenu="return false;" controlsList="nodownload">
+            <video id="videoPlayer" width="50%" controls muted="muted" poster="${curso.cursoId.thumbnail.slice(9)}" oncontextmenu="return false;" controlsList="nodownload">
               <source src="/assets/${curso.cursoId.filePath.slice(9)}" type="video/mp4" />
             </video>
             </div>
